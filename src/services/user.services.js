65e3cef5a9f9814e65c2b126b6d3e4280ca6bb35
Tembogs <temmytizzy201@gmail.com>
2025-10-24 15:45:08 +0100

    update

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,3 +1,4 @@
+import CollectorAssay from "../model/collectorAssay.js";
 import User from "../model/user.js";
 
 
@@ -35,10 +36,36 @@ export const deleteAllUser = async () =>{
   const user = await User.deleteMany()
   return user
 }
-export const updateUser = async(id, updateInfo) => {
-  const user = await User.findByIdAndUpdate(id, updateInfo, {new: true})
-  return user;
-}
+export const updateUser = async (id, updateInfo) => {
+  // Find the user
+  let user = await User.findById(id);
+  if (!user) throw new Error("User not found");
+
+  // If the user is a Collector, sync collector data
+  if (user.role === "Collector") {
+    // Check if collectorAssay already exists for this user
+    let collectorAssay = await CollectorAssay.findOne({ collector: user._id });
+
+    if (collectorAssay) {
+      // Update existing record
+      collectorAssay.serviceArea = updateInfo.location || collectorAssay.serviceArea;
+      await collectorAssay.save();
+    } else {
+      // Create a new record if none exists
+      collectorAssay = new CollectorAssay({
+        user: user._id,
+        collector: user._id,
+        serviceArea: updateInfo.location,
+      });
+      await collectorAssay.save();
+    }
+  }
+
+  // Update the user itself
+  const updatedUser = await User.findByIdAndUpdate(id, updateInfo, { new: true });
+  return updatedUser;
+};
+
 
 export const signOutUser = async (id) => {
   const user = await User.findById(id);
