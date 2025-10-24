4562c2232eb9346cf8bdf920e9d20edfde159eb9
Tembogs <temmytizzy201@gmail.com>
2025-10-24 14:58:14 +0100

    do it

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/", protect, fetchAllUsers)
 router.get("/:id", protect,fetchUserById)
-router.put("/:id", protect,isHouser, editUser)
+router.put("/:id", protect, editUser)
 router.delete("/:id",protect, removeUser)
 router.delete('/', protect, deleteAll)
 router.put('/users/:id/profile-picture', upload.single('profilePicture'), updateProfilePicture);
