166cb56e6173872c7ef2bd74c8b1af68cd634518
Tembogs <temmytizzy201@gmail.com>
2025-11-20 15:32:02 +0100

    adjus

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,6 @@ router.get("/:id", protect,fetchUserById)
 router.put("/:id", protect, editUser)
 router.delete("/:id",protect, removeUser)
 router.delete('/', protect, deleteAll)
-router.put('/users/:id/profile-picture', updateProfilePicture);
+router.put('/:id/profile-picture', updateProfilePicture);
 
 export default router;
