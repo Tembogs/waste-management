54b98d4d0303a36d5c85e728d8f6577df7fcb206
Tembogs <temmytizzy201@gmail.com>
2025-12-03 23:11:21 +0100

    temmy

--- src/services/email.services.js
+++ src/services/email.services.js
@@ -1,28 +1,29 @@
-import { Resend } from "resend";
 import { configDotenv } from "dotenv";
+import nodemailer from "nodemailer";
 
 configDotenv()
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const sendEmail = async (to, subject, html) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail", // tells nodemailer to use Gmail’s SMTP
+            auth: {
+                user: process.env.GMAIL_USER, // your Gmail address
+                pass: process.env.GMAIL_PASS, // app password (not your actual Gmail password!)
+            },
+        });
 
-export const sendEmail = async (to, subject, html) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "WasteWise Nigeria <onboarding@resend.dev>",
-      to,
-      subject,
-      html,
-    });
+        const info = await transporter.sendMail({
+            from: "WasteWise Nigeria <onboarding@tembogs.dev>",
+            to,
+            subject,
+            html,
+        });
 
-    if (error) {
-      console.error("Resend email error:", error);
-      return false;
+        console.log("Message sent: %s", info.messageId);
+    } catch (error) {
+        console.error("Error sending email: ", error);
     }
-
-    console.log("Resend email sent:", data);
-    return true;
-  } catch (err) {
-    console.error("Resend unexpected error:", err);
-    return false;
-  }
 };
+
+export default sendEmail;
