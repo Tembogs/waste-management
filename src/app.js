e521b8b7b9db6143f08c4c2ffab4769435d1f6e6
Tembogs <temmytizzy201@gmail.com>
2026-09-25 23:44:30 +0100

    send

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const app = express();
 dotenv.config();
 setUpMiddlewares(app);
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({ origin: ['https://waste-cleaning-2.vercel.app', 'http://localhost:5173'] }));
 // app.use(cors({ origin: 'http://localhost:5173'}));
 app.use('/api/users', userRoutes);
